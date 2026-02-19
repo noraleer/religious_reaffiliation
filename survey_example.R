947d4f35b7068fc6892f4213362c4e91b997d80c
Nora Lee Reinhardt <nreinhardt@luc.edu>
2026-02-19 16:30:13 -0600

    new

--- survey_example.R
+++ survey_example.R
@@ -6,3 +6,15 @@ sum(apipop$enroll, na.rm=TRUE)
 dstrat<-svydesign(id=~1,strata=~stype, weights=~pw, data=apistrat, fpc=~fpc)
 summary(dstrat)
 svymean(~api00, dstrat)
+
+
+weights <- read.table("customweight_nlsy97_6989725f19c40e6c583.dat")
+
+weights <- weights %>% rename(ID = V1)
+weights <- weights %>% rename(Weight = V2)
+
+weights <- weights %>% mutate(Weight = Weight/100)
+
+dat <- read_csv("FinalCleanedData_long.csv")
+
+final <- left_join(dat, weights, by = "ID")
